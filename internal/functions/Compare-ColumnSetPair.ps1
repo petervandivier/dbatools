bff34bde376908f5858b1e1527db6a60734d2f00
Peter Vandivier <petervandivier@gmail.com>
2019-04-02 09:13:32 +0100

    ez-match .Columns inputs

---
+++ internal/functions/Compare-ColumnSetPair.ps1
@@ -0,0 +1,79 @@
+
+function Compare-ColumnSetPair {
+    <#
+    .SYNOPSIS
+        Given a pair of input arrays, return to the caller a merged array with a confidence value that $source.node[N] corresponds to $target.node[M].
+
+    .DESCRIPTION
+        Internal function to determine INSERT->SELECT mappings.
+
+        When performing a data load, it is necessary to specify FROM->TO mapping(s) for all COLUMN objects between two TABLES. Sometimes it's very straightforward as in...
+
+            INSERT foo ( id, name )
+            SELECT id, name
+            FROM bar;
+
+        ...other times, it is not. Consider...
+
+            INSERT foo ( id, name )
+            SELECT
+                 row_number() over (order by SELECT NULL) AS row_num
+                ,label
+            FROM dbo.Products;
+
+        ...this function attempts to standardize mapping protocol between arbitrary user-supplied arrays for the dbatools module.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]$Source,
+        [Parameter(Mandatory)]
+        [object]$Target
+    )
+
+    function Assert-UniqueLabel {
+        [CmdletBinding()]
+        param (
+            [array]$array
+        )
+        $array.Count -eq ($array | Select-Object -Unique).Count
+    }
+
+    # $Source, $Target | Add-Member -MemberType NoteProperty -Name "IsUniqueLabel" -Value (Assert-UniqueLabel $_.Name)
+
+    $SourceSet = $foo.Columns | Select-Object ID, Name, Identity, Computed, DataType, IsExactMatch
+    $TargetSet = $bar.Columns  | Select-Object ID, Name, Identity, Computed, DataType, IsExactMatch
+
+    $MatchSet = $SourceSet | ForEach-Object {
+        $SourceID = $_.ID
+        $SourceName = $_.Name
+
+        $TargetMatch = $TargetSet | Where-Object Name -EQ $SourceName
+
+        $IsExactMatch = $false
+
+        if ($TargetMatch) {
+            $IsExactMatch = $true
+
+            $TargetID = $TargetMatch.ID
+            $TargetName = $TargetMatch.Name
+        } else {
+
+        }
+
+        $_.IsExactMatch = $IsExactMatch
+        ($TargetSet | Where-Object Name -EQ $SourceName).IsExactMatch = $IsExactMatch
+
+
+        [PSCustomObject]@{
+            SourceID     = $SourceID
+            SourceName   = $SourceName
+            IsExactMatch = $IsExactMatch
+            TargetID     = $TargetID
+            TargetName   = $TargetName
+        }
+    }
+
+    return $MatchSet
+}
