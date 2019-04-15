bd41bdcd154b46dda4abb37ff501e9c3d5ff6bef
Peter Vandivier <petervandivier@gmail.com>
2019-04-15 19:26:11 +0100

    placeholder garbage for multi-table warning (needs tested)

--- internal/functions/ConvertTo-ColumnVector.ps1
+++ internal/functions/ConvertTo-ColumnVector.ps1
@@ -35,48 +35,52 @@ function ConvertTo-ColumnVector {
         # do nothing, the object is well-formed (I hope...)
         # TODO: make this suck less
         # TODO: Handle for [tables[]].Columns input :facepalm:
-    } else {
-        # If object has NoteProperties, attempt to parse them
 
-        $ID = 0
-
-        $return_array = $array | Get-Member | Where-Object MemberType -eq 'NoteProperty' | ForEach-Object {
-            $ID += 1
-
-            [PSCustomObject]@{
-                ID   = $ID
-                Name = $_.Name
+        if (1 -ne ($array.Parent | Select-Object -Unique).Count{
+                Write-Warning "!?!?Multiple Tables!?!?"
             }
-        }
+        } else {
+            # If object has NoteProperties, attempt to parse them
 
-        if (-not($return_array)) {
-            # no NoteProperties were found; object is a list
-            # Assign each leaf as the "Name" value and infer ordinal position
+            $ID = 0
 
-            $return_array = $array | ForEach-Object {
+            $return_array = $array | Get-Member | Where-Object MemberType -eq 'NoteProperty' | ForEach-Object {
                 $ID += 1
 
                 [PSCustomObject]@{
                     ID   = $ID
-                    Name = $_
+                    Name = $_.Name
                 }
+            }
+
+            if (-not($return_array)) {
+                # no NoteProperties were found; object is a list
+                # Assign each leaf as the "Name" value and infer ordinal position
+
+                $return_array = $array | ForEach-Object {
+                    $ID += 1
+
+                    [PSCustomObject]@{
+                        ID   = $ID
+                        Name = $_
+                    }
+                }
+            }
+
+            Remove-Variable ID -ErrorAction SilentlyContinue
+        }
+
+        $return_array = $return_array | ForEach-Object {
+            [PSCustomObject]@{
+                ID           = $_.ID
+                Name         = $_.Name
+                Identity     = $_.Identity
+                Computed     = $_.Computed
+                DataType     = $_.DataType
+                IsExactMatch = [bool]$null
+                IsMapped     = $false
             }
         }
 
-        Remove-Variable ID -ErrorAction SilentlyContinue
+        return $return_array
     }
-
-    $return_array = $return_array | ForEach-Object {
-        [PSCustomObject]@{
-            ID           = $_.ID
-            Name         = $_.Name
-            Identity     = $_.Identity
-            Computed     = $_.Computed
-            DataType     = $_.DataType
-            IsExactMatch = [bool]$null
-            IsMapped     = $false
-        }
-    }
-
-    return $return_array
-}
